d9e7136792997e6c2020bcefde26424ba72b608c
Chao <fxxxchao@gmail.com>
2022-12-08 22:32:37 +0800

    add new hello word project

---
+++ HelloWorld2/HelloWorld2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HelloWorld2
+//
+//  Created by 房超 on 2022/12/8.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
